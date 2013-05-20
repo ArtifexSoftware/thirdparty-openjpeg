61721e8880fd179eec2ebe6694509244561fcd9c
zeniko <zeniko@gmail.com>
2013-05-20 03:55:15 +0200

    fail if opj_jp2_read_header_procedure reads a box claiming to be smaller that its own header (fixes 1851.pdf.SIGSEGV.ce9.948)
    also, tweak the assert added for 2.pdf.SIGFPE.706.1112

--- src/lib/openjp2/tcd.c
+++ src/lib/openjp2/tcd.c
@@ -1655,7 +1655,7 @@ OPJ_BOOL opj_tcd_dc_level_shift_decode ( opj_tcd_t *p_tcd )
                 l_stride = (l_tile_comp->x1 - l_tile_comp->x0) - l_width;
 
                 /* testcase 2.pdf.SIGFPE.706.1112 */
-                assert(l_width + l_stride <= l_tile_comp->data_size / l_height);
+                assert(l_height == 0 || l_width + l_stride <= l_tile_comp->data_size / l_height);
 
                 if (l_img_comp->sgnd) {
                         l_min = -(1 << (l_img_comp->prec - 1));
